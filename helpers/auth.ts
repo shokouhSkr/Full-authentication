359b113844edc34a0d9a4564b352d619b4b2a5e5
shokouhSkr <shokouhskr@gmail.com>
2023-10-02 00:44:50 +0330

    set up prisma with mongodb provider

---
+++ helpers/auth.ts
@@ -0,0 +1,29 @@
+import { NextAuthOptions, getServerSession } from "next-auth";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./connect";
+
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID as string,
+      clientSecret: process.env.FACEBOOK_SECRET as string,
+    }),
+  ],
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
