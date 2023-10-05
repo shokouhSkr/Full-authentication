1bcd0f39c186a06a16a1f2538b59c85530dfbc70
shokouhSkr <shokouhskr@gmail.com>
2023-10-05 19:35:23 +0330

    added passwrod and confirm password with validation

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -1,41 +1,55 @@
 "use client";
 
+import { useState } from "react";
 import Input from "../common/Input";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
-import validator from "validator";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CiUser, CiMail, CiPhone } from "react-icons/ci";
+import { CiUser, CiMail, CiPhone, CiLock } from "react-icons/ci";
 
 type FormSchemaType = z.infer<typeof FormSchema>;
 
 // VALIDATION
-const FormSchema = z.object({
-  first_name: z
-    .string()
-    .min(2, "First name must be at least 2 characters.")
-    .max(32, "First name must be less than 32 characters.")
-    .regex(/^[a-zA-Z]+$/, "No special characters allowed."),
+const FormSchema = z
+  .object({
+    first_name: z
+      .string()
+      .min(2, "First name must be at least 2 characters.")
+      .max(32, "First name must be less than 32 characters.")
+      .regex(/^[a-zA-Z]+$/, "No special characters allowed."),
 
-  last_name: z
-    .string()
-    .min(2, "Last name must be at least 2 characters.")
-    .max(32, "Last name must be less than 32 characters.")
-    .regex(/^[a-zA-Z]+$/, "No special characters allowed."),
+    last_name: z
+      .string()
+      .min(2, "Last name must be at least 2 characters.")
+      .max(32, "Last name must be less than 32 characters.")
+      .regex(/^[a-zA-Z]+$/, "No special characters allowed."),
 
-  email: z.string().email("Please enter a valid email address."),
+    email: z.string().email("Please enter a valid email address."),
 
-  phone: z.string().regex(/^\+98 \(\d{3}\) \d{3}-\d{4}$/, "Please enter a valid phone number"),
-});
+    phone: z.string().regex(/^\+98 \(\d{3}\) \d{3}-\d{4}$/, "Please enter a valid phone number"),
+
+    password: z
+      .string()
+      .min(2, "Password must be at least 6 characters.")
+      .max(52, "Password must be less than 52 characters."),
+
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password doesn't match",
+    path: ["confirmPassword"],
+  });
 
 const RegisterForm = () => {
   const {
     register,
+    watch,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm<FormSchemaType>({
     resolver: zodResolver(FormSchema),
   });
+  const [passwordScore, setPasswordScore] = useState(0);
 
   const onSubmit: SubmitHandler<FormSchemaType> = (data) => console.log(data);
 
@@ -83,6 +97,28 @@ const RegisterForm = () => {
           placeholder="+98 (xxx) xxx-xxxx"
           register={register}
           error={errors?.phone?.message}
+          // error="error's here"
+          disabled={isSubmitting}
+        />
+        <Input
+          name="password"
+          label="Password"
+          type="password"
+          icon={<CiLock />}
+          placeholder="******"
+          register={register}
+          error={errors?.password?.message}
+          // error="error's here"
+          disabled={isSubmitting}
+        />
+        <Input
+          name="confirmPassword"
+          label="Confirm password"
+          type="password"
+          icon={<CiLock />}
+          placeholder="******"
+          register={register}
+          error={errors?.confirmPassword?.message}
           // error="error's here"
           disabled={isSubmitting}
         />
